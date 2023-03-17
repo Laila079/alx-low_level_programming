850326b8e326f9c9f55c127847df8cd0b949ca4a
Laila079 <laila.chrari@gmail.com>
2023-03-17 14:41:51 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * myfunction - writes with printf
+ * Return: 0
+ */
+int myfunction (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
