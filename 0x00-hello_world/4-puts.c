7d3bf1bac63b939f6658ffc41495905769552cea
Laila079 <laila.chrari@gmail.com>
2023-03-17 13:25:10 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+/**
+         *main - This is a description
+        */
 int main(void)
-	/**
-	 *main - This is a description
-	*/
 	{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
