5194f00f2008e46484ba02c1782fa0eb99349f57
Laila079 <laila.chrari@gmail.com>
2023-03-18 16:09:17 -0700

    neg or pos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* my code */
-if (n < 0) {
-	printf ("%d is negative\n", n);
-}
-else if(n > 0) {
-	printf ("%d is positive\n", n);
-}
-else {
-	printf ("%d is zero\n", n);
-}
-}
-	return (0);
+if (n < 0)
+	printf ("%i is negative\n", n);
+else if (n > 0)
+	printf ("%i is positive\n", n);
+else
+	printf ("%i is zero\n", n);
+return (0);
 	}
