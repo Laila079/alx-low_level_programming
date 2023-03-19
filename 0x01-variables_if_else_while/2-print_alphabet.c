8301ad6626fe4460f780911a2c8f0ee2bd85f823
Laila079 <laila.chrari@gmail.com>
2023-03-19 14:22:34 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+/**
+* main - the last digit
+* Return: 0
+*/
+
+int main(void)
+{
+
+int letter = 'a';
+
+while (letter <= 'z') {
+    putchar(letter);
+    letter +=1;
+}
+
+return (0);
+}
