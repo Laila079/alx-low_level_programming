6d477b6a09008dcb87497db35be9081b0bb8562e
Laila079 <laila.chrari@gmail.com>
2023-03-22 13:35:29 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* void print_alphabet_x10 - writes alphabet 10times
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+
+for (i = 1 ; i <= 10 ; i++) 
+{
+	for (c='a'; c<='z', c++)
+	{
+		_putchat(c);
+	}
+	putchar('\n');
+}
+}#include "main.h"
+
+/**
+* void print_alphabet_x10 - writes alphabet 10times
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+
+for (i = 1 ; i <= 10 ; i++) 
+{
+	for (c='a'; c<='z', c++)
+	{
+		_putchat(c);
+	}
+	putchar('\n');
+}
+}
