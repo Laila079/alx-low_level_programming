7ead2e83f58f5ed1d4a2659922063bc036180d21
Laila079 <laila.chrari@gmail.com>
2023-03-22 13:16:32 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet(void) - writes alphabet
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+char c;
+for (c='a'; c<='z', c++)
+{
+_putchat(c);
+}
+putchar('\n');
+}
