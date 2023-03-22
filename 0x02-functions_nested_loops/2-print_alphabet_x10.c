6f31104baed9c0339a332bbf2bbb4e5881c5ce09
Laila079 <laila.chrari@gmail.com>
2023-03-22 13:37:57 -0700

    alphx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,30 +12,10 @@ int i;
 
 for (i = 1 ; i <= 10 ; i++) 
 {
-	for (c='a'; c<='z', c++)
+	for (c='a'; c<='z'; c++)
 	{
-		_putchat(c);
+		_putchar(c);
 	}
-	putchar('\n');
-}
-}#include "main.h"
-
-/**
-* void print_alphabet_x10 - writes alphabet 10times
-* Return: void
-*/
-
-void print_alphabet_x10(void)
-{
-char c;
-int i;
-
-for (i = 1 ; i <= 10 ; i++) 
-{
-	for (c='a'; c<='z', c++)
-	{
-		_putchat(c);
-	}
-	putchar('\n');
+	_putchar('\n');
 }
 }
