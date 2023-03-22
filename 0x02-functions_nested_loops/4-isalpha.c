a937942f616cb603b5e06c9ee2cac86e5c37bd67
Laila079 <laila.chrari@gmail.com>
2023-03-22 15:13:26 -0700

    upper orlower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* int _isalpha - checks for lowercase or uppercase character
+* Return: 1 if c is lowercase or uppercase, and 0 otherwise
+*/
+
+int _isalpha(int c);
+{
+if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+
