a2fba0ab089cebee05becd1ed07a6cd122e916be
Laila079 <laila.chrari@gmail.com>
2023-03-25 05:32:49 -0700

    print all single digit numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
-* main - alphabet exept q and e
+* main - print all single digit numbers
 * Return: 0
 */
 
 int main(void)
 {
 int i = 0;
-while (i < 10, i++)
+while (i < 10)
 {
 printf("%i", i);
+i++;
 }
+printf("\n");
 return (0);
 }
