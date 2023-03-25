456465983976d39a89ac4b6f41c1353538e32146
Laila079 <laila.chrari@gmail.com>
2023-03-24 18:35:14 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
 * print_numbers - writes numbers from 0 to 9
-* Return: 0
+* Return: nothing
 */
 
-void print_numbers(void)
+void print_numbers(void);
 {
 int i;
-for (i = 0; i < 9; i++)
+for (i = 0, i <= 9, i++)
 {
-_putchar(i);
+_putchar(i + 48);
 }
 _putchar('\n');
 }
