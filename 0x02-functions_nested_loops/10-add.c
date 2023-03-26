e2468f7d4a0ba75746c37923487087f81674b538
Laila079 <laila.chrari@gmail.com>
2023-03-25 19:15:22 -0700

    add twi integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - function adds 2 integers
+* @i: first number input
+* @j: second number input
+* Return: result of addition of 2numbers
+*/
+
+int add(int i, int j)
+{
+
+int result;
+result = i + j;
+return (result);
+
+}
