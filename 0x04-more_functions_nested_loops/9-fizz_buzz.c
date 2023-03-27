635b04a27d513f6ad0c7af6728723941c8867fb3
Laila079 <laila.chrari@gmail.com>
2023-03-27 11:32:20 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int i;
-for (i = 0; i < 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
@@ -30,6 +30,7 @@ else
 {
 printf("%i", i);
 printf(" ");
+return (0);
 }
 }
 }
