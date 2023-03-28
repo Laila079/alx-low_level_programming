039e558212e5e1da041c7f094b1e5b38bfe27d68
Laila079 <laila.chrari@gmail.com>
2023-03-27 18:31:50 -0700

    prints even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - input even numbers
+ * @str: char input
+ * Return: 0
+*/
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; ++i)
+{
+if (i % 2 == 0)
+printf("%c", str[i]);
+}
+printf("\n");
+}
