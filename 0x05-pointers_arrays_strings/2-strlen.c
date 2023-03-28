9b137659133d018c5ca2bf90b67843ce84344cc1
Laila079 <laila.chrari@gmail.com>
2023-03-27 17:20:27 -0700

    len of str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>  
+
+#include "main.h"
+
+
+/**
+ * _strlen - strlen of pointer (str)
+ * @s: char pointer
+ * Return: lentgh of str.
+*/
+
+int _strlen(char *s)
+{
+int len;
+
+len = strlen(s);
+
+return (len);
+}
