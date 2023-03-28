5984cd0b28c28abcf95dc8fec463786d032e3971
Laila079 <laila.chrari@gmail.com>
2023-03-27 18:01:30 -0700

    reverse str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverse gear
+ * @s: char input
+ * Return: 0
+*/
+
+void rev_string(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+putchar (s[i]);
+}
+putchar('\n');
+}
