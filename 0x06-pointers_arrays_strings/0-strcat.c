5305c327219b8cf9d1043c6abffca9343f9d25a5
Laila079 <laila.chrari@gmail.com>
2023-03-29 17:52:45 -0700

    contactenate two strings" contactenate "

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strcat - concatenate é strings
+ * @dest: char destination
+ * @src: char source
+ * Return: char result of 2 strings.
+ */
+char *_strcat(char *dest, char *src)
+{
+char *result;
+result = src, dest;
+return (result);
+}
