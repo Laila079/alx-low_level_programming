e5a2c47b1045bcd7987f04ee166364d3a256414a
Laila079 <laila.chrari@gmail.com>
2023-08-03 15:12:08 -0700

    recursion first fuction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar.c;
+void _putchar.c(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,6 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
+o
 #endif
 
