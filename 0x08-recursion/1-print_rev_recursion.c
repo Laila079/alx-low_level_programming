0c5d9e6254fd555c3bb7920d3816e4b9fdc1ae05
Laila079 <laila.chrari@gmail.com>
2023-08-03 16:06:38 -0700

    factorial of n

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_print_rev_recursion  - prints a string in reverse.
+ *@s: input
+ *Return : Always 0
+*/
+
+void _print_rev_recursion(char *s);
+{
+
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *factorial - returns the factorial of a given number.
+ *@n: input integer
+ *Return : -1 if n <0, and the factoriel of n otherwise
+*/
+
+
+int factorial(int n
+{
+if (n < 0)
+return (-1);
+
+else if (n == 0)
+return (1);
+
+else
+return (n * factorial(n - 1));
+}
