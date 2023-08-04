6fabdd685e4abe391da37669cc988ff08089b769
Laila079 <laila.chrari@gmail.com>
2023-08-04 15:30:37 -0700

    create static libmy

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - check if c is uppercase
+* @c: character checked
+* Return: 1 if c is uppercase, and 0 otherwise
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - calculates the absolute value of an integer
+ * @ab: integer 
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
