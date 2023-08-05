2451a96fe6be4013341a016ec9bf16edfc76f7c4
Laila079 <laila.chrari@gmail.com>
2023-08-05 12:41:18 -0700

    argc used to count number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,8 @@
  * Return: 0 always.
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("%d\n", argc);
-	(void)*argv;
-return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
