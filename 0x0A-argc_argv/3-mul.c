a5de2e44b9afa735e0672b8a8bfc1fc965e0fbde
Laila079 <laila.chrari@gmail.com>
2023-08-05 14:36:28 -0700

    mult of two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,35 +2,8 @@
 #include <stdio.h>
 #include "main.h"
 
-int _atoi(char *str)
-{
-int neg;
-int num;
-int i;
-
-i = 0;
-neg = 1;
-num = 0;
-while (str[i] <= ' ')
-i++;
-if (str[i] == '-' || str[i] == '+')
-{
-if (str[i] == '-')
-{
-neg *= -1;
-}
-i++;
-}
-while (str[i] >= '0' && str[i] <= '9')
-{
-num = num * 10 + (str[i] - 48);
-i++;
-}
-return (num * neg);
-}
-
 /**
- * main - a program that multiplies two numbers.
+ * main - a program that multiplies two numbers
  * @argc: integer, its a counter
  * @argv[]: char, containing values
  * Return: 0 always (success)
@@ -38,17 +11,16 @@ return (num * neg);
 
 int main(int argc, char *argv[])
 {
+int i, j;
+
 if (argc < 3 || argc > 3)
 {
 	printf("Error\n");
 	return (1);
 }
 
-int i;
-int j;
-
-i = _atoi(argv[1]);
-j = _atoi(argv[1]);
+	i = _atoi(argv[1]);
+	j = _atoi(argv[1]);
 
 	printf("%d\n", i * j);
 	return (0);
