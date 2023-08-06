e570f93a61377cb8f920035e9ef5a1e057dd5b1e
Laila079 <laila.chrari@gmail.com>
2023-08-06 08:26:06 -0700

    reverse string with recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,7 +7,17 @@
  *Return : Always 0
 */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
+
+if (*s == '\0')
+{
+return;
+}
+
+s++;
+_print_rec_recursion(s);
+s--;
+_putchar(*s);
 
 }
