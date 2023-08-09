f592564f590225e44b8aac5a13c0067cb1f04929
Laila079 <laila.chrari@gmail.com>
2023-08-08 18:11:07 -0700

    new space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,22 +14,21 @@ char *_strdup(char *str)
 char *m;
 int i = 0, size = 0;
 
-if (str == NULL)
-	return (NULL);
+if (str == NUL)
+	return (NUL);
 
-for (; str[size] =! '\0'; size++)
-;
+While(str[size] != '\0')
+size++;
 
-m = malloc(size * sizeof(*str) + 1);
+m = malloc(size * (sizeof(char) + 1));
 
 if (m == 0)
-return (NULL);
+	return (NULL);
 else
 {
-	for (; i < size; i++)
+	for (i = 0; str[i]; i++)
 	m[i] = str[i];
 }
 return (m);
 free(m);
 }
-
